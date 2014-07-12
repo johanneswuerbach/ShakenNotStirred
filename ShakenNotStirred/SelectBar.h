7ba965d478efd14025411ed749559f99b9ede275
Michael Valdez <valdezm@valdezm-macbookair.roam.corp.google.com>
2014-07-12 15:46:09 -0700

    Added Random Number Selector to Project

---
+++ ShakenNotStirred/SelectBar.h
@@ -0,0 +1,13 @@
+//
+//  SelectBar.h
+//  ShakenNotStirred
+//
+//  Created by Michael Valdez on 7/12/14.
+//  Copyright (c) 2014 Slugfest. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SelectBar : NSObject
+-(int) SelectBar (int) objects;
+@end
